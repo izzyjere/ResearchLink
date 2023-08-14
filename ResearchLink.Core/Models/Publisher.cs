4e53125615afa413fc5fe53f0fa7ec54ff8d4c82
izzyjere <wisdomjere5@gmail.com>
2023-08-14 20:33:15 +0200

    New Input

--- ResearchLink.Core/Models/Publisher.cs
+++ ResearchLink.Core/Models/Publisher.cs
@@ -3,8 +3,4 @@
 public class Publisher : Entity
 {
     public string Name { get; set; }
-    public string City { get; set; }
-    public string Region { get; set; }
-    public string PostalCode { get; set; }
-    public string Country { get; set; }
 }

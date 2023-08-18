b4c763870488c46c58c9fd592450a0f91f0917e4
izzyjere <wisdomjere5@gmail.com>
2023-08-18 19:27:06 +0200

    New migration

--- ResearchLink.Core/Migrations/20230818172620_Init13.Designer.cs
+++ ResearchLink.Core/Migrations/20230818172620_Init13.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResearchLink.Core.DataAccess;
 
@@ -11,9 +12,11 @@ using ResearchLink.Core.DataAccess;
 namespace ResearchLink.Core.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230818172620_Init13")]
+    partial class Init13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

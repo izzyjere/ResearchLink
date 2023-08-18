bc9a9a0957756f9ac6ef94fee9eb7374a6f35ac2
izzyjere <wisdomjere5@gmail.com>
2023-08-18 17:55:23 +0200

    More Validations

--- ResearchLink.Core/Models/Article.cs
+++ ResearchLink.Core/Models/Article.cs
@@ -5,7 +5,9 @@ public class Article  : Entity
     public string Title { get; set; }
     [MaxLength(10000)]
     [Required(ErrorMessage = "Please provide an abstract for this paper.")]
-    public string Description { get; set; }     
+    public string Description { get; set; }
+    [Required]
+    [RegularExpression("^((?!00000000-0000-0000-0000-000000000000).)*$", ErrorMessage = "Please select a publisher")]
     public Guid PublisherId { get; set; }
     public virtual Publisher Publisher { get; set; }
     public DateTime DatePublished { get; set; }
@@ -15,7 +17,9 @@ public class Article  : Entity
     public string Pages { get; set; }
     public Guid? VolumeId { get; set; }
     public virtual  Volume? Volume { get; set; }
+    [Required]
+    [Range(1, 4, ErrorMessage = "Please select a research type")]
     public ResearchType Type { get; set; }
-    public FileModel Document { get; internal set; }
+    public FileModel Document { get; set; }
 }
 

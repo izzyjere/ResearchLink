21cb655ddac3890d22ada305276e91db99a87d2e
izzyjere <wisdomjere5@gmail.com>
2023-09-17 17:52:44 +0200

    Introduce Roles

---
+++ ResearchLink/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ResearchLink
+{
+    public static class RoleConstants
+    {
+        public const string Researcher = "RESEARCHER";
+        public const string BasicUsers = "PORTALUSER";
+    }
+}
